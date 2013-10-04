2842ae37ba770148af910aa69db3e5cb96f7fd65
dsevastianov <dsevastianov@zbi.com>
2013-10-04 18:31:05 -0400

    Nuget rstore

---
+++ Tests/ConnectionStringNameTest.fsx
@@ -0,0 +1,19 @@
+﻿printfn "load provider"
+
+#r "../bin/Debug/SqlCommandTypeProvider.dll"
+
+open FSharp.NYC.Tutorial
+
+[<Literal>]
+let queryProductsSql = " 
+SELECT TOP (@top) Name AS ProductName, SellStartDate
+FROM Production.Product 
+WHERE SellStartDate > @SellStartDate
+"
+
+printfn "define type"
+type QueryProductsAsTuples  = SqlCommand<queryProductsSql, ConnectionStringName = "AdventureWorks2012">
+printfn "create command"
+let cmd = QueryProductsAsTuples(top = 7L, SellStartDate = System.DateTime.Parse "2002-06-01")
+//cmd.Execute() |> Async.RunSynchronously |> Seq.iter (fun(productName, sellStartDate) -> printfn "Product name: %s. Sells start date %A" productName sellStartDate)
+
