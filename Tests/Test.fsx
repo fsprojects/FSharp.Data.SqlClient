4265abe56e347422538fb030ef08e71f57f0ccc5
Dmitry Morozov <dmitry.morozov@eikospartners.com>
2013-10-14 15:07:59 -0400

    identations

--- Tests/Test.fsx
+++ Tests/Test.fsx
@@ -11,9 +11,6 @@ let connectionString="Data Source=.;Initial Catalog=AdventureWorks2012;Integrate
 //let connectionString="Server=tcp:lhwp7zue01.database.windows.net,1433;Database=AdventureWorks2012;User ID=jackofshadows@lhwp7zue01;Password=Leningrad1;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;"
 
 [<Literal>]
-let queryTableSql = "SELECT ProductId, Name, SellStartDate FROM Production.Product"
-
-[<Literal>]
 let queryProductsSql = " 
 SELECT TOP (@top) Name AS ProductName, SellStartDate
 FROM Production.Product 
@@ -24,7 +21,9 @@ WHERE SellStartDate > @SellStartDate
 type QueryProductsAsTuples = SqlCommand<queryProductsSql, connectionString>
 let cmd = QueryProductsAsTuples(top = 7L, SellStartDate = System.DateTime.Parse "2002-06-01")
 let result : Async<(string * DateTime) seq> = cmd.Execute()
-result |> Async.RunSynchronously |> Seq.iter (fun(productName, sellStartDate) -> printfn "Product name: %s. Sells start date %A" productName sellStartDate)
+result 
+    |> Async.RunSynchronously 
+    |> Seq.iter (fun(productName, sellStartDate) -> printfn "Product name: %s. Sells start date %A" productName sellStartDate)
 
 //Custom record types
 type QueryProducts = SqlCommand<queryProductsSql, connectionString, ResultType = ResultType.Records>
@@ -45,7 +44,7 @@ result2
 //Single row hint
 type QueryPersonInfoSingletone = SqlCommand<"SELECT * FROM dbo.ufnGetContactInformation(@PersonId)", connectionString, ResultType = ResultType.Records, SingleRow=true>
 let cmd3 = new QueryPersonInfoSingletone(PersonId = 2)
-let result3 = cmd3.Execute() 
+let result3 : Async<QueryPersonInfoSingletone.Record> = cmd3.Execute() 
 result3 
     |> Async.RunSynchronously 
     |> fun x -> printfn "Person info: Id - %i, FirstName - %s, LastName - %s, JobTitle - %s, BusinessEntityType - %s" x.PersonID x.FirstName x.LastName x.JobTitle x.BusinessEntityType
@@ -53,20 +52,22 @@ result3
 //Non-query
 type UpdateEmplInfoCommand = SqlCommand<"EXEC HumanResources.uspUpdateEmployeePersonalInfo @BusinessEntityID, @NationalIDNumber, @BirthDate, @MaritalStatus, @Gender", connectionString>
 let cmd4 = new UpdateEmplInfoCommand(BusinessEntityID = 2, NationalIDNumber = "245797967", BirthDate = System.DateTime(1965, 09, 01), MaritalStatus = "S", Gender = "M")
-cmd4.Execute() |> Async.RunSynchronously 
+let result4 : Async<int> = cmd4.Execute() 
+let rowsAffected = result4 |> Async.RunSynchronously 
 
+//Single value
 type GetServerTime = SqlCommand<"IF @IsUtc = CAST(1 AS BIT) SELECT GETUTCDATE() ELSE SELECT GETDATE()", connectionString, SingleRow=true>
 let getSrvTime = new GetServerTime(IsUtc = true)
-getSrvTime.Execute() |> Async.RunSynchronously |> printfn "%A"
+let result5 : Async<DateTime> = getSrvTime.Execute()
+result5 |> Async.RunSynchronously |> printfn "%A"
 getSrvTime.IsUtc <- false
+//Execute again
 getSrvTime.Execute() |> Async.RunSynchronously |> printfn "%A"
 
+//Stored procedure by name only
 type UpdateEmplInfoCommandSp = SqlCommand<"HumanResources.uspUpdateEmployeePersonalInfo", connectionString, CommandType = CommandType.StoredProcedure>
 let cmdSp = new UpdateEmplInfoCommandSp(BusinessEntityID = 2, NationalIDNumber = "245797967", BirthDate = System.DateTime(1965, 09, 01), MaritalStatus = "S", Gender = "F")
 cmdSp.Execute() |> Async.RunSynchronously
 cmdSp.SpReturnValue
 
-type nonQuery = SqlCommand<"PRINT 'Foo Bar'", connectionString, CommandType = CommandType.Text>
-let cmdNonquery = new nonQuery()
-cmdNonquery.Execute() |> Async.RunSynchronously
 
