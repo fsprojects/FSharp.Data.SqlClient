2456348efb98f3974ebe1797f332380e7fcf22a1
Robert Jeppesen <robert.jeppesen@durius.se>
2013-11-04 19:50:50 +0100

    Remove unused reference, and unused SqlCommand type

--- Tests/TypeProvider.Test.fs
+++ Tests/TypeProvider.Test.fs
@@ -40,7 +40,6 @@ let ConditionalQuery() =
 // If compile fails here, check prereqs.sql
 type TableValuedTuple  = SqlCommand<"exec myProc @x", connectionString, SingleRow = true>
 type TableValuedSprocTuple  = SqlCommand<"myProc", connectionString, SingleRow = true, CommandType = CommandType.StoredProcedure>
-type TableValuedRecord = SqlCommand<"exec myProc @x", connectionString, ResultType = ResultType.Records, SingleRow = true>
 type TableValuedSingle = SqlCommand<"exec SingleElementProc @x", connectionString>
 
 [<Fact>]
