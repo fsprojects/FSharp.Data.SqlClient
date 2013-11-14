2d1affa88dc09d17587ad83d7aaded38b164bdd2
dsevastianov <dsevastianov@gmail.com>
2013-11-14 16:18:43 -0500

    Missing doc files

---
+++ docs/content/index.fsx
@@ -0,0 +1,24 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+(**
+SqlCommandTypeProvider
+===================
+
+Documentation
+
+<div class="row">
+  <div class="span1"></div>
+  <div class="span6">
+    <div class="well well-small" id="nuget">
+      The F# ProjectTemplate library can be <a href="https://www.nuget.org/packages/SqlCommandTypeProvider">installed from NuGet</a>:
+      <pre>PM> Install-Package SqlCommandTypeProvider</pre>
+    </div>
+  </div>
+  <div class="span1"></div>
+</div>
+
+<img src="img/logo.png" alt="SqlCommandTypeProvider" style="float:right;width:150px;margin:10px" />
+*)
