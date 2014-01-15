69cc015508c3fba8cebb9666e45b83c10410b564
Dmitry Morozov <dmitry.morozov@eikospartners.com>
2014-01-14 23:24:06 -0500

    factor out TVP tests

--- src/SqlCommandProvider.Tests/TypeProvider.Test.fs
+++ src/SqlCommandProvider.Tests/TypeProvider.Test.fs
@@ -39,66 +39,6 @@ let ConditionalQuery() =
     Assert.Equal((1, "monkey"), cmd.Execute(flag = 0))    
     Assert.Equal((2, "donkey"), cmd.Execute(flag = 1))    
 
-// If compile fails here, check prereqs.sql
-type TableValuedTuple  = SqlCommand<"exec myProc @x", connectionString, SingleRow = true>
-type MyTableType = TableValuedTuple.MyTableType
-
-[<Fact>]
-let tableValuedTupleValue() = 
-    let cmd = new TableValuedTuple()
-    let x = TableValuedTuple.MyTableType(myId = 5, myName = "test")
-    let p = [
-        MyTableType(myId = 1, myName = "monkey")
-        MyTableType(myId = 2, myName = "donkey")
-    ]
-    Assert.Equal((1, Some "monkey"), cmd.Execute(x = p))    
-
-[<Fact>] 
-let tvpInputIsEnumeratedExactlyOnce() = 
-    let cmd = new TableValuedTuple()
-    let counter = ref 0
-    let x = seq { 
-         counter := !counter + 1
-         yield MyTableType(myId = 1)
-         yield MyTableType(myId = 2, myName = "donkey")
-    }
-    cmd.Execute x |> ignore
-    Assert.Equal(1, !counter)    
-
-[<Fact>] 
-let tableValuedSprocTupleNull() = 
-    let cmd = new TableValuedTuple()
-    let p = [
-        MyTableType(myId = 1)
-        MyTableType(myId = 2, myName = "donkey")
-    ]
-    Assert.Equal((1, None), cmd.Execute p)    
-
-
-type TableValuedSingle = SqlCommand<"exec SingleElementProc @x", connectionString>
-
-[<Fact>]
-let tableValuedSingle() = 
-    let cmd = new TableValuedSingle()
-    let p = [ 
-        TableValuedSingle.SingleElementType(myId = 1) 
-        TableValuedSingle.SingleElementType(myId = 2) 
-    ]
-    let result = cmd.Execute(x = p) |> List.ofSeq
-    Assert.Equal<int list>([1;2], result)    
-
-type TableValuedSprocTuple  = SqlCommand<"myProc", connectionString, SingleRow = true, CommandType = CommandType.StoredProcedure>
-
-[<Fact>]
-let tableValuedSprocTupleValue() = 
-    let cmd = new TableValuedSprocTuple()
-    let p = [
-        TableValuedSprocTuple.MyTableType(myId = 1, myName = "monkey")
-        TableValuedSprocTuple.MyTableType(myId = 2, myName = "donkey")
-    ]
-    let actual = cmd.Execute(p1 = p)
-    Assert.Equal((1, Some "monkey"), actual)    
-
 type ColumnsShouldNotBeNull2 = 
     SqlCommand<"SELECT COLUMN_NAME, IS_NULLABLE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION
             FROM INFORMATION_SCHEMA.COLUMNS
