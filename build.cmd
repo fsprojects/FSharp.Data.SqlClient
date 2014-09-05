dcde09edd6cdd9834911a306e60829404fbaeb87
F# Software Foundation <fsgit@users.noreply.github.com>
2014-09-05 20:04:41 +0100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -3,4 +3,3 @@ if not exist packages\FAKE\tools\Fake.exe (
   .nuget\nuget.exe install FAKE -OutputDirectory packages -Prerelease -ExcludeVersion 
 )
 packages\FAKE\tools\FAKE.exe build.fsx %* 
-pause
