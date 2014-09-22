c1ff22fcacd83268450a899c6438d795cf618a5c
Dmitry Morozov <dmitry.morozov@eikospartners.com>
2014-09-21 22:28:11 -0700

    change DataTable to support IList

--- src/SqlClient/DataTable.fs
+++ src/SqlClient/DataTable.fs
@@ -8,21 +8,24 @@ open System.Collections.Generic
 type DataTable<'T when 'T :> DataRow>() = 
     inherit DataTable() 
 
-    member this.Item index : 'T = downcast this.Rows.[index] 
-
-    interface ICollection<'T> with
+    interface IList<'T> with
         member this.GetEnumerator() = this.Rows.GetEnumerator()
         member this.GetEnumerator() : IEnumerator<'T> = (Seq.cast<'T> this.Rows).GetEnumerator() 
+
         member this.Count = this.Rows.Count
         member this.IsReadOnly = this.Rows.IsReadOnly
+        member this.Item 
+            with get index = downcast this.Rows.[index]
+            and set index row = 
+                this.Rows.RemoveAt(index)
+                this.Rows.InsertAt(row, index)
+
         member this.Add row = this.Rows.Add row
         member this.Clear() = this.Rows.Clear()
         member this.Contains row = this.Rows.Contains row
         member this.CopyTo(dest, index) = this.Rows.CopyTo(dest, index)
+        member this.IndexOf row = this.Rows.IndexOf row
+        member this.Insert(index, row) = this.Rows.InsertAt(row, index)
         member this.Remove row = this.Rows.Remove(row); true
-
-//    later
-//    interface IReadOnlyList<DataRow> with
-//        member this.Item with get index = this.Rows.[index]
-
+        member this.RemoveAt index = this.Rows.RemoveAt(index)
 
