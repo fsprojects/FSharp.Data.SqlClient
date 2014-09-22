8d69bf709570c4b6363f468ad839a56e8b81e663
Dmitry Morozov <dmitry.morozov@eikospartners.com>
2014-09-21 22:36:29 -0700

    replaced interface impl with prop override

--- src/SqlClient/DataTable.fs
+++ src/SqlClient/DataTable.fs
@@ -8,24 +8,28 @@ open System.Collections.Generic
 type DataTable<'T when 'T :> DataRow>() = 
     inherit DataTable() 
 
-    interface IList<'T> with
-        member this.GetEnumerator() = this.Rows.GetEnumerator()
-        member this.GetEnumerator() : IEnumerator<'T> = (Seq.cast<'T> this.Rows).GetEnumerator() 
+    let rows = base.Rows
 
-        member this.Count = this.Rows.Count
-        member this.IsReadOnly = this.Rows.IsReadOnly
-        member this.Item 
-            with get index = downcast this.Rows.[index]
-            and set index row = 
-                this.Rows.RemoveAt(index)
-                this.Rows.InsertAt(row, index)
+    member __.Rows : IList<'T> = {
+        new IList<'T> with
+            member __.GetEnumerator() = rows.GetEnumerator()
+            member __.GetEnumerator() : IEnumerator<'T> = (Seq.cast<'T> rows).GetEnumerator() 
 
-        member this.Add row = this.Rows.Add row
-        member this.Clear() = this.Rows.Clear()
-        member this.Contains row = this.Rows.Contains row
-        member this.CopyTo(dest, index) = this.Rows.CopyTo(dest, index)
-        member this.IndexOf row = this.Rows.IndexOf row
-        member this.Insert(index, row) = this.Rows.InsertAt(row, index)
-        member this.Remove row = this.Rows.Remove(row); true
-        member this.RemoveAt index = this.Rows.RemoveAt(index)
+            member __.Count = rows.Count
+            member __.IsReadOnly = rows.IsReadOnly
+            member __.Item 
+                with get index = downcast rows.[index]
+                and set index row = 
+                    rows.RemoveAt(index)
+                    rows.InsertAt(row, index)
+
+            member __.Add row = rows.Add row
+            member __.Clear() = rows.Clear()
+            member __.Contains row = rows.Contains row
+            member __.CopyTo(dest, index) = rows.CopyTo(dest, index)
+            member __.IndexOf row = rows.IndexOf row
+            member __.Insert(index, row) = rows.InsertAt(row, index)
+            member __.Remove row = rows.Remove(row); true
+            member __.RemoveAt index = rows.RemoveAt(index)
+    }
 
