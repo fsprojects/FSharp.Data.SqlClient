4acea821b17d4fea728b7e27d6fa0e31b6b48df9
Dmitry Morozov <dmitry.morozov@eikospartners.com>
2015-02-23 13:31:14 -0800

    correct problems after cherry-pick

--- src/SqlClient.Tests/CommandTimeoutSetup.fs
+++ src/SqlClient.Tests/CommandTimeoutSetup.fs
@@ -2,24 +2,20 @@
 
 open System.Data.SqlClient
 open Xunit
-open FSharp.Data.CreateCommandFunctorTests
 open FSharp.Data
 open System
 
 [<Literal>]
 let connection = ConnectionStrings.AdventureWorksLiteral
 
+type GetDate = SqlCommandProvider<"select getdate()", connection>
+
  // just make sure we don't assert on the default value in the code whatever it is
 let customTimeout = (new Random()).Next(512, 1024)
 
 let prepareCommandWithConnectionContext =
   use conn = new SqlConnection(connection)
-  conn.Open()
-  let createCommand() =
-    let c = conn.CreateCommand()
-    c.CommandTimeout <- customTimeout
-    c
-  new GetDate(createCommand)
+  new GetDate(conn, commandTimeout = customTimeout)
 
 [<Fact>]
 let ``Setting the command timeout isn't overridden when giving connection context``() =
@@ -43,10 +39,7 @@ let ``Setting the command timeout is reflected when giving connection string``()
 
 [<Fact>]
 let ``Setting the command timeout is reflected when giving a transaction``() =
-    use cx = new SqlConnection(connection)
-    cx.Open()
-    use tx = cx.BeginTransaction()
-    let getDate = new GetDate(tx, customTimeout)
+    let getDate = new GetDate(connection, commandTimeout = customTimeout)
     let sqlCommand = getDate.AsSqlCommand()
     Assert.True(sqlCommand.CommandTimeout = customTimeout)
     let sqlCommand = (getDate :> ISqlCommand).Raw
