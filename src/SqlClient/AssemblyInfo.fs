38ac68ce4b2ea0ae3fa75e300db1ae2e05b403f8
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2015-06-05 09:30:49 -0700

    1.7.0-beta release

--- src/SqlClient/AssemblyInfo.fs
+++ src/SqlClient/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("SqlClient")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data.SqlClient")>]
 [<assembly: AssemblyDescriptionAttribute("SqlClient F# type providers")>]
-[<assembly: AssemblyVersionAttribute("1.6.3")>]
-[<assembly: AssemblyFileVersionAttribute("1.6.3")>]
+[<assembly: AssemblyVersionAttribute("1.7")>]
+[<assembly: AssemblyFileVersionAttribute("1.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.6.3"
+    let [<Literal>] Version = "1.7"
