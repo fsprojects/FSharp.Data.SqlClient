ed161c129cd5a0efa2c2cadd9c439b6481f199b0
Dmitry Morozov <dmitry.a.morozov@gmail.com>
2015-07-05 00:36:46 -0700

    more docs

--- docs/content/data modification.fsx
+++ docs/content/data modification.fsx
@@ -7,7 +7,6 @@ open System
 [<Literal>]
 let connectionString = @"Data Source=.;Initial Catalog=AdventureWorks2014;Integrated Security=True"
 
-
 (**
 
 Data modification
@@ -88,9 +87,9 @@ let recordsAffrected =
 assert(recordsAffrected = 1)
 
 let updatedJobTitle = 
-    // Static Create factory method because it provides better intellisense.
+    // Static Create factory method provides better than ctor intellisense.
     // See https://github.com/Microsoft/visualfsharp/issues/449
-    use cmd = AdventureWorks.dbo.ufnGetContactInformation.Create()
+    use cmd = new AdventureWorks.dbo.ufnGetContactInformation()
 
     //Use ExecuteSingle if you're sure it return 0 or 1 rows
     let result = cmd.ExecuteSingle(PersonID = jamesKramerId) 
@@ -126,7 +125,7 @@ assert (currencyRates.TableName = "[Sales].[CurrencyRate]")
 `TableName property` is set to appropriate value. 
 
 Collection of rows is reachable via `Rows` property of type `IList<#DataRow>`.
-This makes available for typed DataTable familiar list operations: Add, Remove, Insert etc. 
+This makes familiar list operations: Add, Remove, Insert etc. available for typed DataTable. 
 Typed column accessors are added to existing set of `DataRow` type members. 
 It make the intellisense experience a little clunky but it felt important to keep legacy `DataRow`
 type members available for invocation. 
@@ -145,8 +144,11 @@ Method `AddRow` adds a new row to a table.
 
 - There is 1-1 correspondence between column names/types and the method parameters
 - `IDENTITY` column is excluded from parameters list for obvious reasons
-- Columns with `DEFAULT` constraint or accepting `NULL` as a value is type of `option<_>`
-- Extra icing is ability to retrieve `MS_Description` which works only for Sql Server 
+- Nullable columns are mappend to parameters of type `option<_>`
+- Columns with `DEFAULT` constraint are also represented as parameters of type `option<_>`. 
+This is more convenient that specifying DEFAULT as a value in INSERT statement
+- Both kinds of parameters - for nullable columns or columns with defaults - can be omitted from invocation
+- Minor but nice feature is ability to retrieve `MS_Description` which works only for Sql Server 
   because Sql Azure doesn't support extended properties.  
 *)
 do
@@ -156,8 +158,8 @@ do
         ToCurrencyCode = CurrencyCode.``United Kingdom Pound``, 
         AverageRate = 0.63219M, 
         EndOfDayRate = 0.63219M)
-(** Although side-effectful `AddRow` makes easier to add rows in type-safe manner. 
-Pair of invocations to NewRow and Rows.Add can be used as alternative. 
+(** Side-effectful `AddRow` makes easier to add rows in type-safe manner. 
+Pair of invocations to `NewRow` and `Rows.Add` can be used as alternative. 
 This approach also makes sense if for some reason you need to keep a reference to newly added 
 for further manipulations.  
 *)
@@ -184,16 +186,156 @@ do
     let newRow = 
         currencyRates.NewRow(
             CurrencyRateDate = DateTime.Today, 
-            FromCurrencyCode = CurrencyCode.``US Dollar``, 
-            ToCurrencyCode = CurrencyCode.``United Kingdom Pound``, 
+            FromCurrencyCode = "USD", 
+            ToCurrencyCode = "GBP", 
             AverageRate = 0.63219M, 
             EndOfDayRate = 0.63219M
         )
     currencyRates.Rows.Add newRow
+    //Call Update to push changes to a database
     let recordsAffected = currencyRates.Update()
     assert(recordsAffected = 1)
     printfn "ID: %i, ModifiedDate: %O" newRow.CurrencyRateID newRow.ModifiedDate
 
 (**
+- Call to `Update` is required to push changes into a database 
+- `CurrencyRateID` IDENTITY column and all fields with DEFAULT constraints that didn't have value specified are 
+refreshed after update from database. This is very cool feature. **It works only for `BatchSize` = 1** which is default. 
+Of course it's applicable only to new data rows (ones that cause INSERT statement to be issued). 
+Follow [this link](https://msdn.microsoft.com/en-us/library/aadf8fk2.aspx) to find out more about batch updates. 
+
+The snippet below demonstrates update and delete logic. 
+Note how combining SqlCommandProvider to load existing data with typed data tables produces simple and safe code. 
+*)
+
+do
+    use cmd = new SqlCommandProvider<"
+        SELECT * 
+        FROM Sales.CurrencyRate 
+        WHERE FromCurrencyCode = @from
+            AND ToCurrencyCode = @to
+            AND CurrencyRateDate > @date
+        ", connectionString, ResultType.DataReader>()
+    //ResultType.DataReader !!!
+    let currencyRates = new AdventureWorks.Sales.Tables.CurrencyRate()
+    //load data into data table
+    cmd.Execute("USD", "GBP", DateTime(2014, 1, 1)) |> currencyRates.Load
+
+    let latestModification =
+        //manipulate Rows as any sequence
+        currencyRates.Rows
+        |> Seq.sortBy (fun x -> x.ModifiedDate)
+        |> Seq.last
+
+    latestModification.Delete()
+    //or use list operatoin
+    //currencyRates.Rows.Remove latestModification
+
+    //adjust rates slightly
+    for row in currencyRates.Rows do
+        if row.RowState <> System.Data.DataRowState.Deleted
+        then 
+            row.EndOfDayRate <- row.EndOfDayRate + 0.01M
+            row.ModifiedDate <- DateTime.Today
+
+    let totalRecords = currencyRates.Rows.Count
+    // custom batch size - send them all at once
+    let recordsAffected = currencyRates.Update(batchSize = totalRecords) 
+    
+    assert (recordsAffected = totalRecords)
+
+(**
+Custom update logic
+-------------------------------------
+Default update logic provided by typed Data Table can be not sufficient for some advanced scenarios. 
+It doesn't mean you need to give up on convenience of static typing. 
+Customize update behavior by creating you own instance of [SqlDataAdapter](https://msdn.microsoft.com/en-us/library/system.data.sqlclient.sqldataadapter.aspx) 
+and configuring it to your needs. 
+You can find [SqlCommandBuilder](https://msdn.microsoft.com/en-us/library/system.data.sqlclient.sqlcommandbuilder.aspx) class useful for T-SQL generation.
+
+Pseudo code for custom data adapter:
+*)
+
+open System.Data.SqlClient
+
+do
+    let currencyRates = new AdventureWorks.Sales.Tables.CurrencyRate()
+    //load, update, delete, insert rows 
+    use adapter = new SqlDataAdapter()
+    //configure adapter: setup select command, transaction etc.
+    adapter.Update( currencyRates) |> ignore
+    
+(**
+Transaction and connection management
+-------------------------------------
+Please read [Transactions](transactions.html) chapter of the documentation.
+Pay particular attention to *DataTable Updates/Bulk* Load section.
+
+Query-derived tables
+-------------------------------------
+
+There is a way to get your hands on typed data table by specifying ResultType.DataTable as output type 
+for SqlCommandProvider generated command type. 
+This approach gives a flexibility at a cost of leaving more room for error. 
+To begin with an output projection should be suitable for sending changes back to a database. 
+It rules out transformations, extensive joins etc. 
+Basically only raw columns for a single table make good candidates for persistable changes. 
+Typed data table class you get back by executing command with ResultType.DataTable is in large similar to one 
+describe above. One noticeable difference is absence of parametrized AddRow/NewRow method. This is intentional. 
+Updating, deleting or merging rows are most likely scenarios where this can be useful. 
+For update/delete/merge logic to work properly primary key (or unique index) columns must be included 
+into column selection. To insert new records use static data tables types generated by SqlProgrammbilityProvider. 
+That said it’s still possible to add rows with some static typing support. 
+
+One of the example above can be re-written as 
+*)
+
+do
+    //CurrencyRateID is included
+    use cmd = new SqlCommandProvider<"
+        SELECT *   
+        FROM Sales.CurrencyRate 
+        WHERE FromCurrencyCode = @from
+            AND ToCurrencyCode = @to
+            AND CurrencyRateDate > @date
+        ", connectionString, ResultType.DataTable>()
+    //ResultType.DataTable !!!
+    let currencyRates = cmd.Execute("USD", "GBP", DateTime(2014, 1, 1)) 
+
+    let latestModification =
+        currencyRates.Rows
+        |> Seq.sortBy (fun x -> x.ModifiedDate)
+        |> Seq.last
+
+    latestModification.Delete()
+
+    for row in currencyRates.Rows do
+        if row.RowState <> System.Data.DataRowState.Deleted
+        then 
+            row.EndOfDayRate <- row.EndOfDayRate + 0.01M
+            row.ModifiedDate <- DateTime.Today
+
+
+    let newRecord = currencyRates.NewRow()
+    newRecord.CurrencyRateDate <- DateTime.Today
+    newRecord.FromCurrencyCode <- "USD"
+    newRecord.ToCurrencyCode <- "GBP"
+    newRecord.AverageRate <- 0.63219M
+    newRecord.EndOfDayRate <- 0.63219M
+    newRecord.ModifiedDate <- DateTime.Today
+
+    currencyRates.Rows.Add newRecord
+
+    let totalRecords = currencyRates.Rows.Count
+
+    let recordsAffected = currencyRates.Update(batchSize = totalRecords) 
+    printfn "totalRecords: %i, recordsAffected: %i" totalRecords recordsAffected
+    assert (recordsAffected = totalRecords)
+
+(**
+
+<div class="well well-small" style="margin:0px 70px 0px 20px;">
+**WARNING** Unfortunately Update method on typed data table doesn't have asynchronous version. 
+</p></div>
     
 *)
