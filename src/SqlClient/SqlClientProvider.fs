e923aac4464c179f6bc0ecf76ee41f5f4f49bada
Ben Lappin <ben.lappin@insperity.com>
2017-01-06 16:31:49 -0600

    add factory method to SqlProgrammabilityProvider-generated types #252

--- src/SqlClient/SqlClientProvider.fs
+++ src/SqlClient/SqlClientProvider.fs
@@ -210,7 +210,8 @@ type SqlProgrammabilityProvider(config : TypeProviderConfig) as this =
                             cmdProvidedType, 
                             designTimeConfig, 
                             designTimeConnectionString,
-                            config.IsHostedExecution
+                            config.IsHostedExecution,
+                            factoryMethodName = "Create"
                         )
 
                         let executeArgs = DesignTime.GetExecuteArgs(cmdProvidedType, parameters, uddtsPerSchema, unitsOfMeasurePerSchema)
