2aeafc37a31c9d199086fbd22076b79b49e8136e
Vasily Kirichenko <vaskir@gmail.com>
2018-05-13 11:27:18 +0300

    group dependencies
    delete obsolete projects

--- src/SqlClient.Tests/SqlClient.Tests.NET40/Program.fs
+++
@@ -1,6 +0,0 @@
-﻿
-open FSharp.Data
-
-let get42 = new SqlCommandProvider<"SELECT 42", "Server=.;Integrated Security=True">("Server=.;Integrated Security=True")
-get42.Execute() |> Seq.toArray |> printfn "%A" 
-
