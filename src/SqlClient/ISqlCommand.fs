a0f01ab478bab737482be6456fe4b696d0e4825e
Vasily Kirichenko <vaskir@gmail.com>
2018-05-19 13:04:44 +0300

    fix mono

--- src/SqlClient/ISqlCommand.fs
+++ src/SqlClient/ISqlCommand.fs
@@ -185,10 +185,13 @@ type ``ISqlCommand Implementation``(cfg: DesignTimeConfig, connection: Connectio
                 | _ ->
                     match p.SqlDbType with 
                     | SqlDbType.Structured -> 
-                        //done via reflection because not implemented on Mono
-                        let sqlDataRecordType = typeof<SqlCommand>.Assembly.GetType("Microsoft.SqlServer.Server.SqlDataRecord", throwOnError = true)
-                        let records = typeof<Linq.Enumerable>.GetMethod("Cast").MakeGenericMethod(sqlDataRecordType).Invoke(null, [| value |]) :?> seq<Microsoft.SqlServer.Server.SqlDataRecord>
-                        p.Value <- if Seq.isEmpty records then null else records
+                        let records = typeof<Linq.Enumerable>.GetMethod("Cast").MakeGenericMethod(sqlDataRecordType).Invoke(null, [| value |]) 
+                        let hasAny = 
+                            typeof<Linq.Enumerable>
+                                .GetMethods(BindingFlags.Static ||| BindingFlags.Public)
+                                .First(fun m -> m.Name = "Any" && m.GetParameters().Count() = 1)
+                                .MakeGenericMethod(sqlDataRecordType).Invoke(null, [| records |]) :?> bool
+                        p.Value <- if not hasAny then null else records
                     | _ -> p.Value <- value
                             
             elif p.Direction.HasFlag(ParameterDirection.Output) && value :? Array then
