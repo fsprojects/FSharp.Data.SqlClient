7f2c7eb845e6ae0c73ee04abae4580c5334d0381
Gauthier Segay <gauthier.segay@gmail.com>
2018-05-18 18:09:07 -0700

    (minor) fix FAKE path in build.sh

--- build.sh
+++ build.sh
@@ -32,6 +32,6 @@ fi
 run .paket/paket.exe restore
 
 [ ! -e build.fsx ] && run .paket/paket.exe update
-[ ! -e build.fsx ] && run packages/FAKE/tools/FAKE.exe init.fsx
+[ ! -e build.fsx ] && run packages/build/FAKE/tools/FAKE.exe init.fsx
 run packages/build/FAKE/tools/FAKE.exe "$@" $FSIARGS build.fsx
 
