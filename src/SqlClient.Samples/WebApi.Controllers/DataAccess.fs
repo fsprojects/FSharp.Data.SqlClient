bc3062a7adb299de13aecb5593c221d6e19ce00b
Gauthier Segay <gauthier.segay@gmail.com>
2018-05-18 18:05:59 -0700

    introduce paket usage:
    * changes to build.cmd / build.sh to call paket restore
    * fix usages of NuGet.exe in build.fsx
    * fix code issue in samples: WebApi/Controllers/DataAccess.fs
    * defined separate groups in paket.dependencies for build, samples and tests
    ** had to pin FSharp.Core and duplicate it in few groups
    ** had to pin FSharp.TypeProviders.SDK files to old versions as that API has breaking changes in more recent versions
    ** had to pin FAKE, the latest ~4 version fails to build for some weird reason
    All run smoothly from command line and VS2015

--- src/SqlClient.Samples/WebApi.Controllers/DataAccess.fs
+++ src/SqlClient.Samples/WebApi.Controllers/DataAccess.fs
@@ -5,7 +5,7 @@ open FSharp.Data
 [<Literal>]
 let AdventureWorks = "name=AdventureWorks"
 
-type QueryProducts = SqlCommandProvider<"T-SQL/Products.sql", AdventureWorks, DataDirectory = "App_Data">
+type QueryProducts = SqlCommandProvider< const(SqlFile<"T-SQL/Products.sql">.Text), AdventureWorks, DataDirectory = "App_Data">
 
 //type ProductQuery = SqlFile<"T-SQL/Products.sql">
 //type QueryProducts = SqlCommandProvider<ProductQuery.Text, AdventureWorks, DataDirectory = "App_Data">
