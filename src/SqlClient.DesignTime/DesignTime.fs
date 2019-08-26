c7aaa66d54653cd28d14e64d1748b6f6f448385a
Christer van der Meeren <cmeeren@protonmail.com>
2019-08-26 11:54:38 +0200

    Make optional parameters required

--- src/SqlClient.DesignTime/DesignTime.fs
+++ src/SqlClient.DesignTime/DesignTime.fs
@@ -588,7 +588,7 @@ type DesignTime private() =
                         if p.Optional 
                         then 
                             assert(p.Direction = ParameterDirection.Input)
-                            ProvidedParameter(parameterName, parameterType = typedefof<_ option>.MakeGenericType(p.TypeInfo.ClrType) , optionalValue = null)
+                            ProvidedParameter(parameterName, parameterType = typedefof<_ option>.MakeGenericType(p.TypeInfo.ClrType))
                         else
                             if p.Direction.HasFlag(ParameterDirection.Output)
                             then
