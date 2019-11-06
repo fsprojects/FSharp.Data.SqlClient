63c3547aafd23463acc422123cb3182624a4e4be
piaste <piaste@users.noreply.github.com>
2019-11-06 18:43:29 +0100

    Adjusted precision

--- tests/SqlClient.Tests/TypeProviderTest.fs
+++ tests/SqlClient.Tests/TypeProviderTest.fs
@@ -140,10 +140,10 @@ let ``Roundtrip ToTraceString for unicode``() =
     
 [<Fact>]
 let ``Roundtrip ToTraceString for decimals with maximum precision``() = 
-    let cmd = new SqlCommandProvider<"SELECT CAST(@x AS DECIMAL(2, 18))", ConnectionStrings.AdventureWorksNamed>()
-    let decimal_20_18 = 12345678901234567890.123456789012345678m
-    let result = runScalarQuery <| cmd.ToTraceString(decimal_20_18)
-    Assert.Equal(expected = decimal_20_18, actual = unbox<decimal> result)
+    let cmd = new SqlCommandProvider<"SELECT CAST(@x AS DECIMAL(38, 18))", ConnectionStrings.AdventureWorksNamed>()
+    let decimal_38_18 = 12345678901234567890.123456789012345678m
+    let result = runScalarQuery <| cmd.ToTraceString(decimal_38_18)
+    Assert.Equal(expected = decimal_38_18, actual = unbox<decimal> result)
     
 [<Fact>]
 let ``Roundtrip ToTraceString for date time ``() = 
